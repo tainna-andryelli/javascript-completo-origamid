a2e331fa7d8e6af4fbd63cc0a469768654ab4b07
tainna-andryelli <tainnaandryelli@gmail.com>
2024-08-04 22:25:59 -0300

    feat: new Rank Up exercises

--- coding-exercises/codewars/rank-up.js
+++ coding-exercises/codewars/rank-up.js
@@ -55,3 +55,45 @@ function validBraces(braces) {
   return true;
 }
 
+// nFloors = 3
+// [
+//   "  *  ",
+//   " *** ", 
+//   "*****"
+// ]
+
+function towerBuilder(nFloors) {
+  let tower = [];
+  let character = '*';
+  let spaces = ' ';
+  const maxCharacter = 2 * nFloors - 1;
+
+  for(let i = 0; i < nFloors; i++) {
+    let quant = 2 * i + 1;
+
+    if(quant < maxCharacter) {
+      let quantSpaces = (maxCharacter - quant)/2;
+      tower[i] = spaces.repeat(quantSpaces) + character.repeat(quant) + spaces.repeat(quantSpaces);
+    } else {
+      tower[i] = character.repeat(quant);
+    }
+  }
+  return tower;  
+}
+
+// createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]) returns "(123) 456-7890"
+function createPhoneNumber(numbers){
+  let phoneNumber = '';
+  for(let i = 0; i < numbers.length; i++) {
+    if(i == 0) {
+      phoneNumber += '(';
+    } else if (i == 3) {
+      phoneNumber += ') ';
+    } else if (i == 6) {
+      phoneNumber += '-';
+    }
+    phoneNumber += numbers[i];
+  }
+
+  return phoneNumber;
+}
